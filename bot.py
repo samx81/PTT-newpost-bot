e8556d124cf0d03a07aaed73db39e5aa75476846
Sam/Manjaro <sam89881@gmail.com>
2020-03-16 00:43:16 +0800

    + Now can Cancel Job

--- bot.py
+++ bot.py
@@ -5,27 +5,16 @@ import logging
 import scraper
 from telegram.ext import CommandHandler, MessageHandler, Filters
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.DEBUG)
+
+
 config = configparser.ConfigParser()
 config.read('config.ini')
-updater = Updater(token=config['telegram-bot']['token'],use_context=True)
-dispatcher = updater.dispatcher
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-job = updater.job_queue
-#TODO: 手動取消
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-# echo_handler = MessageHandler(Filters.text, echo)
-# dispatcher.add_handler(echo_handler)
-
-# TODO: 記住重複項
 def callback_post_check(context: CallbackContext):
     scr_obj = scraper.getTopicList(context.job.context['boardname'])
     
@@ -42,13 +31,36 @@ def callback_post_check(context: CallbackContext):
         context.bot.send_message(
             chat_id = context.job.context['id'], 
             text= ''.join([scr_obj['content'],"\ncheck input"]))
-def callback_post_set(update:Update, context: CallbackContext):
-    job_check = job.run_repeating(callback_post_check, interval=5,
-    context={'id':update.message.chat_id,'boardname':context.args[0],'prev':''})
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Job set.")
+def callback_post_set(update:Update, context: CallbackContext):
+    if len(context.args) != 0:
+        job.run_repeating(callback_post_check, interval=5,first=0,
+            context={'id':update.effective_chat.id,'boardname':context.args[0],'prev':''})
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="check args.")
+
+def callback_job_cancel(update:Update, context: CallbackContext):
+    job.stop()
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Job canceled.")
+
+#TODO: 檢查狀態
+#TODO: 特定詞過濾
+
+### Bot set-up ###
+
+# replace token you got
+updater = Updater(token=config['telegram-bot']['token'],use_context=True)
+
+dispatcher = updater.dispatcher
+job = updater.job_queue
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
 #TODO: 時間 arg
 check_handler = CommandHandler('check', callback_post_set)
+cancel_handler = CommandHandler('cancel',callback_job_cancel)
 dispatcher.add_handler(check_handler)
+dispatcher.add_handler(cancel_handler)
+
 updater.start_polling()
