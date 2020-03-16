55a12e3e898feaad5463e229703f492203c2b676
Sam/Manjaro <sam89881@gmail.com>
2020-03-16 18:01:24 +0800

    add exclude terms feature, tidy up var name

--- bot.py
+++ bot.py
@@ -8,43 +8,65 @@ from telegram.ext import CommandHandler, MessageHandler, Filters
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.DEBUG)
 
-
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+DEFAULT_INTEVAL = 10
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 def callback_post_check(context: CallbackContext):
-    scr_obj = scraper.getTopicList(context.job.context['boardname'])
+    scarp_args = context.job.context
+    newly_scrap = scraper.getNewPost(scarp_args['boardname'])
     
-    if scr_obj['status']: 
-        prev_scr_link = context.job.context['prev']
-        if prev_scr_link == scr_obj['content']:
-            context.bot.send_message(chat_id=context.job.context['id'], text= '重複')
-        else:
-            context.job.context['prev']= scr_obj['content']
-            context.bot.send_message(chat_id=context.job.context['id'], text= scr_obj['content'])
+    if newly_scrap['status']: 
+
+        # 檢查最新貼文與上次是否相同
+        if scarp_args['prev'] == newly_scrap['url']:
+            return
+            # context.bot.send_message(chat_id=context.job.context['id'], text= '重複')
+        # 檢查排除關鍵字
+        elif scarp_args['exclude']:
+            for term in scarp_args['exclude']:
+                if term in newly_scrap['title']:
+                    scarp_args['prev'] = newly_scrap['url']
+                    return
+        
+        output_str = "{}\nhttps://www.ptt.cc{}"
+        output_str = output_str.format(newly_scrap['title'],newly_scrap['url'])
+
+        scarp_args['prev'] = newly_scrap['url']
+        context.bot.send_message(chat_id=scarp_args['id'], text=output_str)
     else:
         context.job.schedule_removal()
         #TODO: 完善敘述
         context.bot.send_message(
-            chat_id = context.job.context['id'], 
-            text= ''.join([scr_obj['content'],"\ncheck input"]))
+            chat_id = scarp_args['id'], 
+            text= ''.join([newly_scrap['content'],"\ncheck input"]))
 
 def callback_post_set(update:Update, context: CallbackContext):
-    if len(context.args) != 0:
-        job.run_repeating(callback_post_check, interval=5,first=0,
-            context={'id':update.effective_chat.id,'boardname':context.args[0],'prev':''})
-    else:
+    # 不正確的參數就排除
+    if not context.args or len(context.args)>3:
         context.bot.send_message(chat_id=update.effective_chat.id, text="check args.")
+
+    # 由後依序處理參數
+    else:
+        excludeList = context.args[2].split('/') if len(context.args)==3 else ''
+
+        input_interval = context.args[1] if isinstance(context.args[1],int) else DEFAULT_INTEVAL
+        if input_interval == DEFAULT_INTEVAL:
+            context.bot.send_message(chat_id=update.effective_chat.id, text="USE DEFAULT.")
+
+        scarp_args ={'id':update.effective_chat.id, 'boardname':context.args[0],'exclude':excludeList, 'prev':''}
+        job.run_repeating(callback_post_check, interval=input_interval, first=0, context=scarp_args)
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Job set.")
 
 def callback_job_cancel(update:Update, context: CallbackContext):
     job.stop()
     context.bot.send_message(chat_id=update.effective_chat.id, text="Job canceled.")
 
-#TODO: 檢查狀態
-#TODO: 特定詞過濾
+#TODO: 檢查JOB狀態
 
 ### Bot set-up ###
 
@@ -57,7 +79,6 @@ job = updater.job_queue
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-#TODO: 時間 arg
 check_handler = CommandHandler('check', callback_post_set)
 cancel_handler = CommandHandler('cancel',callback_job_cancel)
 dispatcher.add_handler(check_handler)
--- scraper.py
+++ scraper.py
@@ -6,19 +6,18 @@ from bs4 import BeautifulSoup
 ptt = 'https://www.ptt.cc/bbs/{}/index.html'
 header = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
 
-def getTopicList(boardname:str):
+def getNewPost(boardname:str):
     try:
         html = requests.get(ptt.format(boardname),header)
         html.raise_for_status()
         bs = BeautifulSoup(html.text,'html.parser')
-        lateset = bs.find(class_='r-list-sep').find_previous_sibling()
-        title = lateset.find(class_='title')
 
-        output_str = "{}\nhttps://www.ptt.cc{}".format(
-            title.a.get_text(),title.a.get('href'))
-        return({'status':True, 'content':output_str})
+        lateset = bs.find(class_='r-list-sep').find_previous_sibling()
+        titleItem = lateset.find(class_='title')
+
+        return({'status':True, 'title':titleItem.a.get_text(), 'url':titleItem.a.get('href')})
     except requests.exceptions.HTTPError as e:
-        return({'status':False, 'content':str(e.response)})
+        return({'status':False, 'title':str(e.response)})
 
     
 # boardname = input("輸入看板")
